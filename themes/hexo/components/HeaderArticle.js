889890c84266c71a9af1d237a2a3e1a999237574
siuze <windfall@hnu.edu.cn>
2022-11-22 17:14:11 +0800

    Update HeaderArticle.js

--- themes/hexo/components/HeaderArticle.js
+++ themes/hexo/components/HeaderArticle.js
@@ -24,11 +24,11 @@ export default function HeaderArticle({ post, siteInfo }) {
       <header className="animate__slideInDown animate__animated bg-black bg-opacity-70 absolute top-0 w-full h-96 py-10 flex justify-center items-center ">
         <div className='mt-24'>
           {/* 文章Title */}
-          <div className="font-bold text-xl shadow-text flex justify-center text-center text-white dark:text-white ">
+          <div className="font-bold md:text-5xl shadow-text flex justify-center text-center text-white dark:text-white ">
             {post.title}
           </div>
 
-          <section className="flex-wrap shadow-text flex text-sm justify-center mt-2 text-white dark:text-gray-400 font-light leading-8">
+          <section className="flex-wrap shadow-text flex text-sm justify-center mt-2 text-white dark:text-gray-400 font-light leading-8" style="margin-top: 2rem;">
             <div className='dark:text-gray-200'>
               {post.category && <>
                 <Link href={`/category/${post.category}`} passHref>
