ff43cd53271eff27ad932f4e3159a9b29295659f
siuze <windfall@hnu.edu.cn>
2022-11-23 13:12:50 +0800

    调整首页布局

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -75,7 +75,7 @@ const Header = props => {
       className="duration-500 md:bg-fixed w-full bg-cover bg-center h-screen bg-black text-white"
       style={{
         backgroundImage:
-          `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0,0,0,0.3), rgba(0,0,0,0.2), rgba(0,0,0,0.3), rgba(0, 0, 0, 0.6) ),url("${siteInfo?.pageCover}")`
+          `linear-gradient(rgba(0, 0, 0, 0.03), rgba(0,0,0,0), rgba(0,0,0,0), rgba(0,0,0,0), rgba(0, 0, 0, 0) ),url("${siteInfo?.pageCover}")`
       }}
     >
       <div className="absolute flex flex-col h-full items-center justify-center w-full ">
