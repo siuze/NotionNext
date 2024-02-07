d2342e6c4f27f6fbd34b2b0c1bd693d25ebf4353
siuze <windfall@hnu.edu.cn>
2024-02-08 02:02:56 +0800

    调整页脚样式，增加CDN说明

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -12,11 +12,15 @@ const Footer = ({ title }) => {
       style={{ marginTop: '30px'}}
     >
       {/* <DarkModeButton/> */}
-      <div style={{ position: 'absolute', width: "100%", zIndex: 0 }}>
+      <div style={{ position: 'absolute', width: "100%" }}>
         {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br /></>}
         <a href={siteConfig('LINK')} className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</a><br />
         <span className='text-xs pl-2 hidden busuanzi_container_site_uv'><i className='text-xs fas fa-users' /> <span className='text-xs px-1 busuanzi_value_site_uv'> </span> </span><span className='text-xs hidden busuanzi_container_site_pv'><i className='text-xs fas fa-eye' /><span className='text-xs px-1 busuanzi_value_site_pv'> </span>  </span><br />
-        <i className='far fa-copyright' /> {`${copyrightDate}`} <a href={siteConfig('LINK')} className='text-xs pt-4 text-light-400 dark:text-gray-400'>{siteConfig('AUTHOR')}</a>. All Rights Reserved. <p className='text-xs pt-2 text-light-500 dark:text-gray-500'><i className='mx-1 fas fa-code' />Powered by <a href='https://github.com/siuze/NotionNext' className='dark:text-gray-300'>NotionNext {siteConfig('VERSION')}</a>.</p>
+        <i className='far fa-copyright' /> {`${copyrightDate}`} <a href={siteConfig('LINK')} className='text-xs pt-4 text-light-400 dark:text-gray-400'>{siteConfig('AUTHOR')}</a>. All Rights Reserved. 
+        <div style={{ position: 'relative', width: "100%", zIndex: 100 }}>
+        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'><i className='mx-1 fas fa-code' />Powered by <a href='https://github.com/siuze/NotionNext' className='dark:text-gray-300'>NotionNext {siteConfig('VERSION')}</a>.</p>
+        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'><i className='mx-1 fas fa-bolt' />Acceleration with <a href='https://blogCDN.net' className='dark:text-gray-300'>blogCDN </a>.</p>
+      </div>
       </div>
       <Fish />
     </footer>
