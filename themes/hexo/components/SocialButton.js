13caba3ae6bfbef217ffe49987d0ef444d91f005
siuze <windfall@hnu.edu.cn>
2024-03-07 01:02:12 +0800

    修复订阅按钮的链接路径错误，微调样式

--- themes/hexo/components/SocialButton.js
+++ themes/hexo/components/SocialButton.js
@@ -6,7 +6,7 @@ import { siteConfig } from '@/lib/config'
  * @constructor
  */
 const SocialButton = () => {
-  return <div className='w-full justify-center flex-wrap flex'>
+  return <div className='w-full justify-center flex-wrap flex pb-2'>
     <div className='space-x-3 text-xl text-gray-600 dark:text-gray-300 '>
       {siteConfig('CONTACT_GITHUB') && <a target='_blank' rel='noreferrer' title={'github'} href={siteConfig('CONTACT_GITHUB')} >
         <i className='transform hover:scale-125 duration-150 fab fa-github dark:hover:text-indigo-400 hover:text-indigo-600'/>
@@ -29,7 +29,7 @@ const SocialButton = () => {
       {siteConfig('CONTACT_EMAIL') && <a target='_blank' rel='noreferrer' title={'email'} href={`mailto:${siteConfig('CONTACT_EMAIL')}`} >
         <i className='transform hover:scale-125 duration-150 fas fa-envelope dark:hover:text-indigo-400 hover:text-indigo-600'/>
       </a>}
-      {JSON.parse(siteConfig('ENABLE_RSS')) && <a target='_blank' rel='noreferrer' title={'邮件订阅与RSS'} href={'/subscribe'} >
+      {JSON.parse(siteConfig('ENABLE_RSS')) && <a target='_blank' rel='noreferrer' title={'邮件订阅与RSS'} href={'/article/subscribe'} >
         <i className='transform hover:scale-125 duration-150 fas fa-rss dark:hover:text-indigo-400 hover:text-indigo-600'/>
       </a>}
       {siteConfig('CONTACT_BILIBILI') && <a target='_blank' rel='noreferrer' title={'bilibili'} href={siteConfig('CONTACT_BILIBILI')} >
